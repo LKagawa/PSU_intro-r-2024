c9ad17fda253d20737074df10aece552e03c09f8
LKagawa <leia.kagawa@odot.oregon.gov>
2024-09-11 15:15:19 -0700

    ggplot examples

---
+++ R/intro_ggplot2.R
@@ -0,0 +1,47 @@
+# libraries & data ----
+library(dplyr)   # functions
+library(ggplot2) # graphing
+library(readxl)  # excel
+
+df_ice <- read_xlsx("data/icebreaker_answers.xlsx") # data
+
+# plotting ----
+tt_mi_fig <- df_ice |> # Figure of travel time in Miles
+  ggplot(
+    aes(x = travel_time, # aesthetics: x & y definitions
+        y = travel_distance)
+        ) + # '+' adds a layer to th plot thus far
+    geom_point()# geometry points
+
+tt_mi_fig # displays the figure
+
+# with color & relabled axsis
+tt_mi_ox_fig <- df_ice |>
+  ggplot(
+    aes(x = travel_time,
+        y = travel_distance,
+        color = serial_comma)
+   ) +
+  geom_point() +
+  xlab("Travel Time") +
+  ylab("Travel Distance")
+
+tt_mi_ox_fig
+
+# changed background/theme of figure
+tt_mi_fig <- tt_mi_fig +
+  theme_bw()
+
+tt_mi_fig
+
+# create figure using mode instead of serial_comma
+tt_mi_mode_fig <- df_ice |>
+  ggplot(
+    aes(x = travel_time,
+        y = travel_distance,
+        color = travel_mode))+
+  geom_point() +
+  xlab("Travel Time") +
+  ylab("Travel Distance")
+
+tt_mi_mode_fig
