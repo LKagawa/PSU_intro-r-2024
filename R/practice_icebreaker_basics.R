fdbfbbca9b627e79b341391cf52d6a590c7c07ee
LKagawa <leia.kagawa@odot.oregon.gov>
2024-09-11 12:13:49 -0700

    did an assignment

--- R/practice_icebreaker_basics.R
+++ R/practice_icebreaker_basics.R
@@ -2,17 +2,38 @@
 # Don't forget: Comment anywhere the code isn't obvious to you!
 
 # Load the readxl and dplyr packages
+library(readxl)
+library(dplyr)
 
 # Use the read_excel function to load the class survey data
+survey_results <- read_excel("data/icebreaker_answers.xlsx")
 
 # Take a peek!
+View(survey_results)
 
 # Create a travel_speed column in your data frame using vector operations and 
 #   assignment
+survey_results$travel_mph <- (
+  survey_results$travel_distance / 
+  (survey_results$travel_time / 60) # MPH
+  )
 
 # Look at a summary of the new variable--seem reasonable?
+summary(survey_results)
+boxplot(survey_results$travel_mph ~ survey_results$travel_mode)
 
-# Choose a travel mode, and use a pipe to filter the data by your travel mode
+# Choose a travel mode, and use a pipe to filter the data by your travels
+results_bike <- survey_results |>
+  filter(
+    travel_mode == "bike"
+  )
+summary(results_bike)
+
+results_car <- survey_results |>
+  filter(
+    travel_mode == "car"
+  )
+summary(results_car)
 
 # Note the frequency of the mode (# of rows returned)
 
@@ -22,6 +43,10 @@
 #   reasonable?
 
 # Filter the data by some arbitrary time, distance, or speed threshold
-
+survey_results |> 
+  filter(
+    travel_mph > 20 & travel_mph < 50
+    )
 # Stretch yourself: Repeat the above, but this time filter the data by two 
 #   travel modes (Hint: %in%)
+survey_results |> 
